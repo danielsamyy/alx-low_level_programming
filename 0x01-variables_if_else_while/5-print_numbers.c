3990e1720484f13920c69a6a19ac94368d28dfa1
danielsamyy <danielsamy205@gmail.com>
2023-10-30 09:33:29 -0700

    var,loop

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+/**
+ * main - program that prints the alphabet in lowercase and upper case
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		printf("%i", digit);
+		digit++;
+	}
+	printf("\n");
+
+	return (0);
+}
