08f71f842f59601d3a6bb0fd9f07fea7b3274fb6
danielsamyy <danielsamy205@gmail.com>
2024-01-29 10:31:16 -0800

    dynamic_libraries

---
+++ 0x18-dynamic_libraries/myfunctions.c
@@ -0,0 +1,82 @@
+#include "main.h"
+#include <stdio.h>
+
+int _putchar(char c) {
+    return putchar(c);
+}
+
+int _islower(int c) {
+    // Add implementation for _islower function
+}
+
+int _isalpha(int c) {
+    // Add implementation for _isalpha function
+}
+
+int _abs(int n) {
+    // Add implementation for _abs function
+}
+
+int _isupper(int c) {
+    // Add implementation for _isupper function
+}
+
+int _isdigit(int c) {
+    // Add implementation for _isdigit function
+}
+
+int _strlen(char *s) {
+    // Add implementation for _strlen function
+}
+
+void _puts(char *s) {
+    // Add implementation for _puts function
+}
+
+char *_strcpy(char *dest, char *src) {
+    // Add implementation for _strcpy function
+}
+
+int _atoi(char *s) {
+    // Add implementation for _atoi function
+}
+
+char *_strcat(char *dest, char *src) {
+    // Add implementation for _strcat function
+}
+
+char *_strncat(char *dest, char *src, int n) {
+    // Add implementation for _strncat function
+}
+
+char *_strncpy(char *dest, char *src, int n) {
+    // Add implementation for _strncpy function
+}
+
+int _strcmp(char *s1, char *s2) {
+    // Add implementation for _strcmp function
+}
+
+char *_memset(char *s, char b, unsigned int n) {
+    // Add implementation for _memset function
+}
+
+char *_memcpy(char *dest, char *src, unsigned int n) {
+    // Add implementation for _memcpy function
+}
+
+char *_strchr(char *s, char c) {
+    // Add implementation for _strchr function
+}
+
+unsigned int _strspn(char *s, char *accept) {
+    // Add implementation for _strspn function
+}
+
+char *_strpbrk(char *s, char *accept) {
+    // Add implementation for _strpbrk function
+}
+
+char *_strstr(char *haystack, char *needle) {
+    // Add implementation for _strstr function
+}
